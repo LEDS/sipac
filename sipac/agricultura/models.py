16afd5d30a12089aa1f5db3bae464123828a3fb3
paulossjunior <paulossjunior@gmail.com>
2016-01-08 20:25:45 -0200

    Adicionando o modelo de produção

--- sipac/agricultura/models.py
+++ sipac/agricultura/models.py
@@ -1,1 +1,37 @@
 from django.db import models
+from core.models import Municipio, MicroRegiao,MesoRegiao
+
+class Produto(models.Model):
+
+    nome = models.CharField(max_length=100)
+
+    codigo = models.IntegerField()
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        ordering = ['nome']
+
+
+class Producao(models.Model):
+
+    ano = models.IntegerField()
+
+    mes = models.IntegerField()
+
+    preco = models.FloatField()
+
+    produto = models.ForeignKey(Produto,on_delete=models.CASCADE)
+
+    municipio = models.ForeignKey(Municipio,on_delete=models.CASCADE)
+
+    microregiao = models.ForeignKey(MicroRegiao,on_delete=models.CASCADE)
+
+    mesoregiao = models.ForeignKey(MesoRegiao,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.ano + self.mes
+
+    class Meta:
+        ordering = ['ano','mes']
